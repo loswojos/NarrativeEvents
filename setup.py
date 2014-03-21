8fceb0dd6f29dd251fb24462dc58f6bea3f38f91
Michael Wojcieszek <mw2353@columbia.edu>
2014-03-21 17:54:02 -0400

    Added descriptions

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name = 'narrative',
     packages = ['narrative'],
     version = '0.0.2',
-    description = 'A python library for computing narrative chains.',
-    author='Mike Wojcieszek',
+    description = 'A python library for building narrative chains.',
+    author='Michael Wojcieszek',
     author_email='mw2353@gmail.com',
     url='https://github.com/loswojos/NarrativeEvents',
     install_requires=[
