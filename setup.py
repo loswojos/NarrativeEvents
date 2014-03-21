4a4291c6d8bd22da9b9ae3b9613b3eda7f8031a0
kedz <kedzie@cs.columbia.edu>
2014-03-21 01:56:54 -0400

    Fixed setup.py module location.:

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name = 'narrative',
-    packages = ['.'],
-    version = '0.0.1',
+    packages = ['narrative'],
+    version = '0.0.2',
     description = 'A python library for computing narrative chains.',
     author='Mike Wojcieszek',
     author_email='mw2353@gmail.com',
